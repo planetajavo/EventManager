16b666b28fca218c37b4a83b144aa833a4758f5a
planetajavo <planetajavo@yahoo.es>
2014-03-10 11:25:11 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ironevents::Application.config.secret_key_base = 'fd5e495112757ef0aba1a2ce4b4103ea347a68c358b2ec549a410f4e5a02dff5f32f0f8d2c794312d722a0524e0b01b58d91aef2640cdb1db699a2daa0a55ef2'
